97adafdf81169cbd52fe5e18bf31af58bcaf784f
TangAndy <andytang285@gmail.com>
2024-03-19 15:12:04 -0500

    All buttons in the home screen (except menu) should move to their respective screens. Also created the layout files and java classes for the other screens.

---
+++ app/src/main/java/com/example/marxteamproject/NotesActivity.java
@@ -0,0 +1,14 @@
+package com.example.marxteamproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notes_screen);
+    }
+}
