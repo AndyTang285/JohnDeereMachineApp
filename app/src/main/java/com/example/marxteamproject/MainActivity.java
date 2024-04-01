f33cc2af2c4544fe170e6feb999c8e7e2c79dcf1
WadeRonit <ronitjw@gmail.com>
2024-04-01 14:54:44 -0500

    new login

--- app/src/main/java/com/example/marxteamproject/MainActivity.java
+++ app/src/main/java/com/example/marxteamproject/MainActivity.java
@@ -1,10 +1,23 @@
 package com.example.marxteamproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.jetbrains.annotations.ApiStatus;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_screen);
+        setContentView(R.layout.activity_main);
+
+
     }
 }
