23b14dfb263c17225e4a91d3d92d6a06bede0665
143868 <kaiikaii5754@gmail.com>
2024-04-04 15:22:20 -0500

    Added pulling Images From FireBase Storage

--- app/src/main/java/com/example/marxteamproject/TractorScreen.java
+++ app/src/main/java/com/example/marxteamproject/TractorScreen.java
@@ -8,11 +8,18 @@ import androidx.appcompat.app.AppCompatActivity;
 public class TractorScreen extends AppCompatActivity {
 
    public ImageButton button;
+   public String tractortype;
+   public String ModelNum;
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.tractor_screen);
         FireBaseStorage test = new FireBaseStorage();
         super.onCreate(savedInstanceState);
        test.mymethod(findViewById(R.id.Tractor_image_1), this);
+       Database test2 = new Database();
+       tractortype = "Combines";
+       ModelNum = "S650 (2014)";
+       Database.Tractor(tractortype, ModelNum);
+
        /* button = findViewById(R.id.image_back_button);
 
 
