baf228ee2884a47d451823fcade6d17ff9b6845b
TangAndy <andytang285@gmail.com>
2024-04-04 15:18:06 -0500

    need to connect notes to firebase

--- app/src/main/java/com/example/marxteamproject/NotesDetailedActivity.java
+++ app/src/main/java/com/example/marxteamproject/NotesDetailedActivity.java
@@ -2,13 +2,42 @@ package com.example.marxteamproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class NotesDetailedActivity extends AppCompatActivity {
+    EditText titleEditText;
+    EditText contentEditText;
+    ImageButton saveNotesBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notes_detailed);
+        setContentView(R.layout.notes_detailed);
+
+        titleEditText = findViewById(R.id.notes_title_text);
+        contentEditText = findViewById(R.id.notes_content_text);
+        saveNotesBtn = findViewById(R.id.save_notes_btn);
+
+        saveNotesBtn.setOnClickListener( (v)-> saveNote());
+    }
+
+
+    public void saveNote() {
+        String noteTitle = titleEditText.getText().toString();
+        String noteContent = contentEditText.getText().toString();
+
+        if (noteTitle==null || noteContent.isEmpty()) {
+            titleEditText.setError("Please enter a title");
+            return;
+        }
+
+        Intent intent = new Intent(NotesDetailedActivity.this, NotesActivity.class);
+        startActivity(intent);
+
+
+
     }
 }
