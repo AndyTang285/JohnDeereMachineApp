9a84998f49c6ff3ef36e7591df4b861e95e2c3ea
TangAndy <andytang285@gmail.com>
2024-04-18 15:19:08 -0500

    hi again

--- app/src/main/java/com/example/marxteamproject/MapActivity.java
+++ app/src/main/java/com/example/marxteamproject/MapActivity.java
@@ -1,14 +1,43 @@
 package com.example.marxteamproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
-public class MapActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap gMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_screen);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_fragment);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        LatLng location = new LatLng(41.574446, -90.5304021);
+        MarkerOptions options = new MarkerOptions().position(location).title("Malevolent Shrine");
+
+        options.icon(BitmapDescriptorFactory.fromResource(R.drawable.sukuna));
+        googleMap.addMarker(options);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 20));
+
+    }
+    public void ScaleDrawable (Drawable drawable, int gravity, float scaleWidth, float scaleHeight) {
+
     }
 }
