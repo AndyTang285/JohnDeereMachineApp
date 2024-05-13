68bd379d7b38832181637ba260704e3417703130
Xavier <xavier@ballards.org>
2024-05-13 14:30:45 -0500

    Combined AndySprint2 and Ronit/Xavi's code.

--- app/src/main/java/com/example/marxteamproject/HomeActivity.java
+++ app/src/main/java/com/example/marxteamproject/HomeActivity.java
@@ -59,9 +59,6 @@ public class HomeActivity extends AppCompatActivity {
         categoriesBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                Intent intent = new Intent(HomeActivity.this, TractorScreen.class);
-                startActivity(intent);
                 Intent intent = new Intent(HomeActivity.this, CategoriesActivity.class);
                 startActivity(intent);
 
