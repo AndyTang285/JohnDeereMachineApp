1a076ac2fd17cd79478b02ab8880119e27a3f0f8
Andy Tang <andytang285@gmail.com>
2024-05-14 01:46:00 -0500

    custom pins

--- app/src/main/java/com/example/marxteamproject/MapActivity.java
+++ app/src/main/java/com/example/marxteamproject/MapActivity.java
@@ -21,9 +21,11 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.AdvancedMarkerOptions;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -44,10 +46,13 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -80,6 +85,8 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     DocumentReference docRef = db.collection("tractors").document("default");
     CollectionReference home = docRef.collection("home");
     CollectionReference work = docRef.collection("work");
+
+    CollectionReference pins = db.collection("pins");
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -165,6 +172,20 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
                     Log.i("XDXD", tractorID + "Latitude: " + tractorLatitude + " " + "Longitude: " + tractorLongitude);
                     LatLng tractorLocation = new LatLng(tractorLatitude, tractorLongitude);
                     map.addMarker(new MarkerOptions().position(tractorLocation).title(tractorName));*/
+                }
+            }
+        });
+
+        pins.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                for (QueryDocumentSnapshot document: queryDocumentSnapshots) {
+                    String pinID = document.getId();
+                    String pinName = document.getString("name");
+                    double pinLatitude = document.getDouble("latitude");
+                    double pinLongitude = document.getDouble("longitude");
+                    LatLng pinLocation = new LatLng(pinLatitude, pinLongitude);
+                    map.addMarker(new MarkerOptions().position(pinLocation).title(pinName).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
                 }
             }
         });
@@ -254,19 +275,21 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
 
-        /*map.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+        //click and hold for a few seconds, then create a pin will show up
+        map.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
             @Override
             public void onMapLongClick(@NonNull LatLng latLng) {
                 handler.removeCallbacks(runnable);
-                map.addMarker(new AdvancedMarkerOptions().position(latLng));
-                Intent intent  = new Intent(MapActivity.this, CreatePinActivity.class);
-                startActivity(intent);
+                showPinPopup(findViewById(R.id.map_fragment), latLng);
             }
-        });*/
+        });
+
 
         //when clicked, finds current location again
         currentLocationBtn.setOnClickListener((v -> handler()));
 
+
+        //when map dragged, stop always taking user back to current  location
         map.setOnCameraMoveStartedListener(new GoogleMap.OnCameraMoveStartedListener() {
             @Override
             public void onCameraMoveStarted(int i) {
@@ -274,22 +297,17 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
             }
         });
 
-        map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-            @Override
-            public void onMapClick(@NonNull LatLng latLng) {
-                handler.removeCallbacks(runnable);
-                showPinPopup(findViewById(R.id.map_fragment), latLng);
-            }
-        });
-
     }
 
+    //Pin popup
     private void showPinPopup(View view, LatLng latLng) {
         LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View pinPopup = inflater.inflate(R.layout.create_pin_screen, null);
         PopupWindow popupWindow = new PopupWindow(pinPopup, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT,true);
         popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
 
+
+        //when user clicks cancel button, the popup closes and no pin is created
         pinPopup.findViewById(R.id.cancel_pin_btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -297,17 +315,45 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
             }
         });
 
+
+        //when user clicks create pin, a pin gets created, and name gets stored.
         pinPopup.findViewById(R.id.create_pin_btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                pinNameText = pinPopup.findViewById(R.id.pin_name_edit_text);
                 String pinName = pinNameText.getText().toString();
-                MarkerOptions marker = new MarkerOptions().position(latLng).title(pinName);
+                MarkerOptions marker = new MarkerOptions().position(latLng).title(pinName).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
                 map.addMarker(marker);
+                // Store the pin in Firestore
+                storePinInFirestore(latLng, pinName);
                 popupWindow.dismiss();
             }
         });
     }
+
+    private void storePinInFirestore(LatLng latLng, String pinName) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference pinsCollection = db.collection("pins");
+
+        Map<String, Object> pinData = new HashMap<>();
+        pinData.put("latitude", latLng.latitude);
+        pinData.put("longitude", latLng.longitude);
+        pinData.put("name", pinName);
+
+        pinsCollection.add(pinData).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Log.d("Firestore", "Pin stored successfully: " + documentReference.getId());
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.e("Firestore", "Error storing pin: " + e.getMessage());
+            }
+        });
+    }
+
 
 
 }
